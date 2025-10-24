a7f883424d87a4bec343bd3980dc98013d8dcf0f
superbook830 <marlsoriano096@gmail.com>
2025-10-24 19:35:52 +0800

    Update RemoteAPISource.js

--- ModularMediaStreamingSuite/sources/RemoteAPISource.js
+++ ModularMediaStreamingSuite/sources/RemoteAPISource.js
@@ -1,6 +1,15 @@
 class RemoteAPISource {
   constructor() {
-    this.stream = "https://www.w3schools.com/html/mov_bbb.mp4";
+    // These are direct-access MP4 demo streams (CORS-safe)
+    const demoVideos = [
+      "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+      "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+      "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4",
+      "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"
+    ];
+
+    // Pick one at random each time
+    this.stream = demoVideos[Math.floor(Math.random() * demoVideos.length)];
   }
 
   getStream() {
